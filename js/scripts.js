3efc60a3fe590ed48187be05ac5e96988859d66e
Rajas Paunikar <rajaspaunikar2716@gmail.com>
2021-06-16 19:00:36 +0530

    npm scripts part 1

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function()
+{
+    $('#mycarousel').carousel({interval:20000});
+    $('#carouselButton').click(function()
+    {
+        if($('#carouselButton').children('span').hasClass('fa-pause'))
+        {
+            $('#mycarousel').carousel('pause');
+            $('#carouselButton').children('span').removeClass('fa-pause');
+            $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else
+        {
+            $('#mycarousel').carousel('cycle');
+            $('#carouselButton').children('span').removeClass('fa-play');
+            $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    $('#reservebutton').click(function()
+    {
+        $('#reservemodal').modal('show');
+    });
+    $('#loginbutton').click(function()
+    {
+        $('#loginModal').modal('show')
+    });
+});
